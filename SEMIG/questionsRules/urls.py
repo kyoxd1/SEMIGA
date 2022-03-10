b61464debc16f27f7663a08251330d54eb4c58f8
kyoxd1 <kyomaster1234@gmail.com>
2022-03-09 20:00:27 -0400

    addGorgojoTemplateAndStyles

--- SEMIG/questionsRules/urls.py
+++ SEMIG/questionsRules/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("", views.index, name= "index"),
     path("<int:question_id>/rules/", views.rules, name= "rules"),
     path("<int:question_id>/resp/", views.resp, name= "resp"),
-    path("gorgojo_info/", views.gorgojoInformation, name= "gorgojoInformation"),
+    path("<int:question_id>/gorgojo_info/", views.gorgojoInformation, name= "gorgojoInformation"),
     # /questionsRules/5
     path("<int:pk>/", views.DetailView.as_view(), name= "detail"),
     # /questionsRules/results
--- SEMIG/questionsRules/views.py
+++ SEMIG/questionsRules/views.py
@@ -43,7 +43,13 @@ def rules(request, question_id):
     if(question_id == 6):
         latest_question_list = Question.objects.filter(pk__in=[6,7,8,9,10])
     if(question_id == 11):
-        latest_question_list = Question.objects.filter(pk__in=[11,12,13,8,14])
+        latest_question_list = Question.objects.filter(pk__in=[11,12,13,10,8,14])
+    if(question_id == 15):
+        latest_question_list = Question.objects.filter(pk__in=[11,12,13,14,15,16,17])
+    if(question_id == 18):
+        latest_question_list = Question.objects.filter(pk__in=[16,17,18])
+    if(question_id == 19):
+        latest_question_list = Question.objects.filter(pk__in=[19,20,21,22])
     return render(request, "questionsRules/rules.html", {
     "latest_question_list": latest_question_list,
     "question": question
@@ -58,17 +64,11 @@ def resp(request, question_id ):
             question2 = request.POST['question2']
             latest_question_list = Question.objects.filter(pk__in=[3,4])
             if(question1=='No' and question2 == 'No'):
-                return render(request, "questionsRules/gorgojoInformation.html", {
-                    "latest_question_list": latest_question_list
-                })
+                return HttpResponseRedirect(reverse("questionsRules:gorgojoInformation", args=(question.id,)))
             elif(question1=='Si' and question2 == 'Si'):
-                return render(request, "questionsRules/gorgojoInformation.html", {
-                    "latest_question_list": latest_question_list
-                })
+                return HttpResponseRedirect(reverse("questionsRules:gorgojoInformation", args=(question.id,)))
             elif(question1=='No' and question2 == 'Si'):
-                return render(request, "questionsRules/gorgojoInformation.html", {
-                    "latest_question_list": latest_question_list
-                })
+                return HttpResponseRedirect(reverse("questionsRules:gorgojoInformation", args=(question.id,)))
             elif(question1 == 'Si' and question2== 'No' ):
                 question = get_object_or_404(Question, pk = 3)
                 return HttpResponseRedirect(reverse("questionsRules:rules", args=(question.id,)))
@@ -81,9 +81,10 @@ def resp(request, question_id ):
             question1 = request.POST['question3']
             question2 = request.POST['question4']
             if((question1=='si' and question2 == 'Si') or (question1=='no' and question2=='Si')):
-                return render(request, "questionsRules/gorgojoInformation.html", {
-                    "latest_question_list": latest_question_list
-                })
+                # return render(request, "questionsRules/gorgojoInformation.html", {
+                #     "latest_question_list": latest_question_list
+                # })
+                return HttpResponseRedirect(reverse("questionsRules:gorgojoInformation", args=(question.id,)))
             else:
                 question = get_object_or_404(Question, pk = 5)
                 return HttpResponseRedirect(reverse("questionsRules:rules", args=(question.id,)))
@@ -91,8 +92,45 @@ def resp(request, question_id ):
             question1 = request.POST['question5']
             if(question1 == 'Recién planeo empezar mi cultivo'):
                 question = get_object_or_404(Question, pk = 6)
-            else:
+            elif(question1 == 'Acabo de sembrar'):
                 question = get_object_or_404(Question, pk = 11)
+            elif(question1 == 'Mi cultivo de papa, ya esta más de la mitad'):
+                question = get_object_or_404(Question, pk = 15)
+            elif(question1 == 'Estoy por cosechar mi cultivo'):
+                question = get_object_or_404(Question, pk = 18)
+            elif(question1 == 'Acabo de cosechar mi cultivo'):
+                question = get_object_or_404(Question, pk = 19)
+        elif(question.id == 6):
+            question1 = request.POST['question6']
+            question2 = request.POST['question7']
+            question3 = request.POST['question8']
+            question4 = request.POST['question9']
+            question5 = request.POST['question10']
+        elif(question.id == 11):
+            question1 = request.POST['question11']
+            question2 = request.POST['question12']
+            question3 = request.POST['question13']
+            question4 = request.POST['question8']
+            question5 = request.POST['question14']
+            if(question1 == 'No'):
+                return HttpResponseRedirect(reverse("questionsRules:gorgojoInformation", args=(question.id,)))
+        elif(question.id == 15):
+            question1 = request.POST['question11']
+            question2 = request.POST['question12']
+            question3 = request.POST['question13']
+            question4 = request.POST['question14']
+            question5 = request.POST['question15']
+            question6 = request.POST['question16']
+            question7 = request.POST['question17']
+        elif(question.id == 18):
+            question1 = request.POST['question16']
+            question2 = request.POST['question17']
+            question3 = request.POST['question18']
+        elif(question.id == 19):
+            question1 = request.POST['question19']
+            question2 = request.POST['question20']
+            question3 = request.POST['question21']
+            question4 = request.POST['question22']
                 
     except (ValueError, KeyError, Question.DoesNotExist, Choices.DoesNotExist):
         if(question_id == 1 or question_id == 2):
@@ -105,6 +143,12 @@ def resp(request, question_id ):
             latest_question_list = Question.objects.filter(pk__in=[6,7,8,9,10])
         if(question_id == 11):
             latest_question_list = Question.objects.filter(pk__in=[11,12,13,8,14])
+        if(question_id == 15):
+            latest_question_list = Question.objects.filter(pk__in=[11,12,13,14,15,16,17])
+        if(question_id == 18):
+            latest_question_list = Question.objects.filter(pk__in=[16,17,18])
+        if(question_id == 19):
+            latest_question_list = Question.objects.filter(pk__in=[19,20,21,22])
         return render(request, "questionsRules/rules.html",{
             "latest_question_list": latest_question_list,
             "error_message": "Debes elegir alguna respuesta",
@@ -114,10 +158,28 @@ def resp(request, question_id ):
         return HttpResponseRedirect(reverse("questionsRules:rules", args=(question.id,)))
     
     
-def gorgojoInformation(request, latest_question_list):
+def gorgojoInformation(request, question_id):
     # latest_question_list = Question.objects.filter(pk__in=[3,4])
+    question = get_object_or_404(Question, pk=question_id)
+    if(question_id == 1 or question_id == 2):
+        latest_question_list = Question.objects.filter(pk__in=[1,2])
+    if(question_id == 3 or question_id == 4):
+        latest_question_list = Question.objects.filter(pk__in=[3,4])
+    if(question_id == 5):
+        latest_question_list = Question.objects.filter(pk__in=[5])
+    if(question_id == 6):
+        latest_question_list = Question.objects.filter(pk__in=[6,7,8,9,10])
+    if(question_id == 11):
+        latest_question_list = Question.objects.filter(pk__in=[11,12,13,8,14])
+    if(question_id == 15):
+        latest_question_list = Question.objects.filter(pk__in=[11,12,13,14,15,16,17])
+    if(question_id == 18):
+        latest_question_list = Question.objects.filter(pk__in=[16,17,18])
+    if(question_id == 19):
+        latest_question_list = Question.objects.filter(pk__in=[19,20,21,22])
     return render(request, "questionsRules/gorgojoInformation.html", {
-        "latest_question_list": latest_question_list
+        "latest_question_list": latest_question_list,
+        "question": question
         });
     
 class DetailView(generic.DetailView):
