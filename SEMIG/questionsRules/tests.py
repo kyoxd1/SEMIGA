3977f15e45c4ea7ec2816aaeae00526b962613fe
kyoxd1 <kyomaster1234@gmail.com>
2022-03-17 21:13:04 -0400

    FirstTest

--- SEMIG/questionsRules/tests.py
+++ SEMIG/questionsRules/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+"""
+_Test Question Model_
+"""
+class QuestionModelTests(TestCase):
+    
+    def test_get_wanted_question_or_questions(self):
+        """Question: Get wanted question or questions of Model
+        retrun False for value is distinct of expected question 
+        """
+        question = Question.objects.get(pk = 1)
+        text_question = "¿Conoces al Gorgojo de Los Andes?" 
+        self.assertEqual(question.question_text, text_question)
