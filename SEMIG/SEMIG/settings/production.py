91876d947670245d95356887474d1dae1a541723
AlejoLuis <kyomaster1234@gmail.com>
2023-09-17 22:48:06 -0400

    config

--- SEMIG/SEMIG/settings/production.py
+++ SEMIG/SEMIG/settings/production.py
@@ -7,9 +7,14 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER' : 'postgres',
+        'PASSWORD' : 'C7x5g6GXa0uRd05BN23h',
+        'HOST' : 'containers-us-west-61.railway.app',
+        'PORT' : '5858',   
     }
 }
